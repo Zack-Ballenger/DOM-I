83d8be2c19dd70f50088e20d58a52124c65b6ce7
Zack-Ballenger <zack.ballenger@tutanota.com>
2020-10-31 02:19:29 -0600

    Finally about finished up

--- js/index.js
+++ js/index.js
@@ -68,18 +68,55 @@ navLinks[3].textContent = "Features";
 navLinks[4].textContent = "About";
 navLinks[5].textContent = "Content";
 
+
 const button = document.getElementsByTagName("button");
 button[0].textContent = "Get Started";
 
+const midPageStuff = document.getElementsByTagName("h4");
+midPageStuff[0].textContent = "Features";
+midPageStuff[1].textContent = "About";
+midPageStuff[2].textContent = "Services";
+midPageStuff[3].textContent = "Product";
+midPageStuff[4].textContent = "Vision";
+midPageStuff[5].textContent = "Contact"
 
-// const domIsAwesome = document.getElementsByTagName("h1");
-// domIsAwesome[0].textContent = "Dom Is Awesome";
-
+const midPageText = document.getElementsByTagName("p");
+midPageText[0].textContent = "blah blooh blah blah blah words words words so many words ok here are just a few more words and now we're done";
+midPageText[1].textContent = "ok here are some more words oh man so many words it's like the words never end but now they end ok bye";
+midPageText[2].textContent = "I will quote from the eminent genius of a man I greatly respect: Bawitdaba, da bang, da dang diggy diggy, diggy, said the boogie, said up jump the boogie"; 
+midPageText[3].textContent = "Honestly I'm only writing this out instead of copy/pasting from the original.html file because I wanna be the very best typeosipher"
+midPageText[4].textContent = "I mean where's the fun in just copying some old dead latin book everywhere you go? I'll tell you where. Nowhere. That's where the fun is. Nowhere."
+midPageText[5].textContent = "The corner of 4th and Broadmoore";
+midPageText[6].textContent = "(806) 867-5309";
+midPageText[7].textContent = "PM me dawg";
 
 //## Task 3: Add new content
-//* [ ] Change the color of the navigation text to be green.
+//* [Yep] Change the color of the navigation text to be green.
 //* [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
 //* [ ] Check your work by looking at the [original html](original.html) in the browser
+
+const navColors = document.querySelectorAll("a");
+navColors.forEach(function(a) {
+  a.style.color = "green";
+});
+
+// var parent = document.createElement("div");
+// var p = document.createElement("p");
+// var span = document.createElement("span");
+// parent.append(p);
+// parent.prepend(span);
+
+const firstAnchor = document.createElement("a");
+const secondAnchor = document.createElement("a");
+const nav = document.querySelector("nav")
+
+nav.prepend(firstAnchor);
+nav.appendChild(secondAnchor);
+firstAnchor.href = "#";
+firstAnchor.innerHTML = "Entre";
+secondAnchor.href = "https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMjE0NjQ2My9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY1NzQwMjE0OH0.60NI2zybyoMvEQHfTaatIOqjWVKG29EmxlumqOwADMI/img.jpg?width=1200&coordinates=0%2C234%2C0%2C166&height=800"
+secondAnchor.innerHTML = "Le Fin";
+
 
 //## Stretch Goals
 //* [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
